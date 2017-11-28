84a59ea89b53a602f3e76d097c812768f909731b
公主的觅风 <19881962@qq.com>
2017-11-28 23:56:25 +0800

    添加了一点注释

--- geturl.py
+++ geturl.py
@@ -1,22 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
 import re
-
+# soup.find_all里面的回调函数
 def has_data(tag):
     return tag.has_attr('data-title-no')
-
+# 通过打开本地文件获取文件名列表
 def get_name(name):
 	fo = open(name,"r",encoding="UTF-8")
 	lists = []
 	for line in fo.readlines():
 		lists.append(line.strip())
 	return lists
-
+# 把爬到的更新的漫画名字写回本地文件
 def save_name(name,sets):
 	fo = open(name,"w")
 	for up in sets:
 		fo.write(up+"\n")
-
+# 连接webstoon并爬数据
 def get_bs(lists):
 	sets = set([]);
 	r = requests.get("https://www.dongmanmanhua.cn/genre")
